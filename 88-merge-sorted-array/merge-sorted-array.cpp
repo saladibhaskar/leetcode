d491cbb2d8e014757f8712d4f4d3028140b56a05
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-01-16 18:36:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (41.49%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,34 +1,38 @@
-class Solution {
-public:
-    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
-       int i=0;
-        int j=0;
-        vector<int>v;
-        while(i<m && j<n)
-        {
-            if(n1[i]<=n2[j]){
-                v.push_back(n1[i]);
-                i+=1;
-            }
-            else{
-                v.push_back(n2[j]);
-                j+=1;
+ class Solution {
+    public:
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+            vector<int>nums;
+            int i=0;
+            int j=0;
+            int k=n;
+           while (!nums1.empty() && nums1.back() == 0&&k!=0) {
+            nums1.pop_back();
+            k-=1;
+        }
+            while(i<nums1.size() && j<n){
+                
+                if(nums1[i]<nums2[j]){
+                    
+                    nums.push_back(nums1[i]);
+                    i++;
                 }
-        }
-        while(i<m)
-        {
-            v.push_back(n1[i]);
-            i+=1;}
-        while(j<n)
-        {
-            v.push_back(n2[j]);
-            j++;
-        }
-        i=0;
-        while(i<v.size())
-        {
-            n1[i]=v[i];
+                else{
+                    nums.push_back(nums2[j]);
+                    j++;
+                }
+            }
+            while (i < nums1.size()) {
+              
+            nums.push_back(nums1[i]);
             i++;
-        }
-    }
-};
+            }
+            while (j < n) {
+            nums.push_back(nums2[j]);
+            j++;
+            }
+            nums1.clear();
+            for(int w=0;w<nums.size();w++){
+            nums1.push_back(nums[w]);
+            }
+        }
+    };
