0e995df0a46557ac4d30ed156b4e73179f7b508c
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-01-17 11:54:03 +0530

    Time: 3 ms (52.57%) | Memory: 7.6 MB (61.59%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& a) {
+    int i;
+    vector<int>v;
+    vector<vector<int>>res;
+    cout<<"hii";
+    for(i=0;i<pow(2,a.size());i++)
+    {
+        int j=0;
+        int num=i;
+          while(num!=0)
+          {
+              if(num&1)
+              {
+                v.insert(v.begin(),a[j]);
+               // cout<<a[j]<<" ";
+              }
+              num=num>>1;
+              //cout<<num;
+              j++;
+          }
+          res.push_back(v);
+          
+          v.clear();  
+    }
+    return res;
+    }
+};
