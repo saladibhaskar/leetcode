ce0931077af60f9cb7a864ebf7e9ef8e7e3c35fb
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-01-30 23:47:53 +0530

    Time: 30 ms (30.77%) | Memory: 75.9 MB (11.01%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -10,52 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+ListNode* rev(ListNode*node)
+{
+    ListNode *cur=node;
+    ListNode *pre=NULL;
+    ListNode* nxt=NULL;
+    while(cur)
     {
-        stack<int >st1;
-        stack<int >st2;
-        while(l1)
+        nxt=cur->next;
+        cur->next=pre;
+        pre=cur;
+        cur=nxt;
+    }
+    return pre;
+}
+    ListNode* addTwoNumbers(ListNode* t1, ListNode* t2) 
+    {
+        ListNode *l1=rev(t1);
+        ListNode *l2=rev(t2);
+        int carry=0;
+        ListNode* head=new ListNode();
+        ListNode*temp=head;
+        while(l1!=NULL or l2!=NULL or carry!=0)
         {
-            st1.push(l1->val);
-            l1=l1->next;
-        }
-        while(l2)
-        {
-            st2.push(l2->val);
-            l2=l2->next;
-        }
-        int car=0;
-        ListNode *res=new ListNode(-1);
-        while(!st1.empty() || !st2.empty()||car!=0)
-        {
-            int a=0,b=0;
-           if(!st1.empty())
-           {
-           a=st1.top();
-           st1.pop();
-           }
-           if(!st2.empty())
-           {
-           b=st2.top();
-           st2.pop();
-           }
-           int c=a+b+car;
-           car=c/10;
-           int s=c%10;
-           ListNode *nn=new ListNode(s);
-           if(res->val==-1)
-           {
-               res->val=nn->val;
-           }
-           else
-           {
-               nn->next=res;
-               res=nn;
-           }
+            int sum=0;
+            if (l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if (l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            sum=sum+carry;
+            carry=sum/10;
+            int rem=sum%10;
 
-
+            ListNode* ele=new ListNode(rem);
+            temp->next=ele;
+            temp=temp->next;
         }
-        return res;
+        return rev(head->next);
 
     }
 };
