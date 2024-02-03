d84745be5dcdd985421137233d6de7da938e7b43
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-02-04 00:30:34 +0530

    Time: 33 ms (47.97%) | Memory: 75.6 MB (20.04%) - LeetSync

--- 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.cpp
+++ 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.cpp
@@ -9,10 +9,8 @@ public:
         else if(sum1<sum2) return 2;
         return 0;   
        }
-       if (p1[0]==10) sum1+=p1[1]*2;
-       else sum1+=p1[1];
-       if (p2[0]==10) sum2+=p2[1]*2;
-       else sum2+=p2[1];
+       sum1+=(p1[0]==10)?p1[1]*2:p1[1];
+       sum2+=(p2[0]==10)?p2[1]*2:p2[1];
         for(i=2;i<p1.size();i++){
 
             sum1+=(p1[i-1]==10 || p1[i-2]==10)?p1[i]*2:p1[i];
