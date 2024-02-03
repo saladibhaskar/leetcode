9c5e97b6072cd25cefe224f8fe376d4bbe6af479
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-02-04 00:31:59 +0530

    Time: 29 ms (69.37%) | Memory: 75.6 MB (12.84%) - LeetSync

--- 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.cpp
+++ 2684-determine-the-winner-of-a-bowling-game/determine-the-winner-of-a-bowling-game.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int isWinner(vector<int>& p1, vector<int>& p2) {
-       int i=2;
+       int i;
        int sum1=p1[0],sum2=p2[0];
-       if(p1.size()==1 && p2.size()==1)
+       if(p1.size()==1)
        {
         if(sum1>sum2) return 1;
         else if(sum1<sum2) return 2;
