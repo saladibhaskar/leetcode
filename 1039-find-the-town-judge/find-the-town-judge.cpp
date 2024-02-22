f3d0ec77f1d9e465b6bb8c759c2eff953edbb808
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-02-22 18:39:48 +0530

    Time: 118 ms (88.83%) | Memory: 64.3 MB (69.38%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& t) {
-        unordered_map<int,int>mp;
-        unordered_map<int,int>mp2;
-       // for(int i=0;i<t.size();i++) mp2[t[i][0]]+=1;
-        for(int i=0;i<t.size();i++)
-        {
-            mp[t[i][1]]+=1;
-           // mp2.erase(t[i][0]);
-           mp2[t[i][0]]+=1;
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>people(n+1,0);
+         for(int i=0;i<trust.size();i++){
+            people[trust[i][1]]++;
+            people[trust[i][0]]--;
         }
-        int c=0,i;
-        //if(mp2.size()==0)return -1;
-        //else return mp2.begin()->first;
-        for( i=1;i<=n;i++)
+        int l=-1,cnt=0;
+        for(int i=1;i<=n;i++)
         {
-            if(mp[i]==n-1 && mp2.find(i)==mp2.end())
-            {
-                c=1;
-                return i;
+            if(l==-1 && people[i]==n-1){
+                l=i;
+                cnt++;
             }
+            if(cnt>1) return -1;
         }
-        if(c==0) return -1;
-        return i;
+        return l;
+
     }
 };
