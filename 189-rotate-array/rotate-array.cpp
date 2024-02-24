3ebf585b3d9d9b202c30e48165e0393cbf0d475a
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-02-24 21:13:46 +0530

    Time: 14 ms (96.65%) | Memory: 27.3 MB (32.95%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -5,8 +5,8 @@ public:
         int r=nums.size()-k;
         reverse(nums.begin()+r,nums.end());
         reverse(nums.begin(),nums.begin()+r);
-         reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.end());
         
-        for(int i=0;i<nums.size();i++)cout<<nums[i]<<" ";
+        //for(int i=0;i<nums.size();i++)cout<<nums[i]<<" ";
     }
 };
