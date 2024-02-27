98431e64b59b69b192e6d9e0bab43cf9e888c650
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-02-27 21:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (43.50%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -7,15 +7,12 @@ int sumof(int n)
 }
     bool isHappy(int n) {
         int c=0;
-        while(n!=1)
+        while(n!=1 && n!=7)
         {
             n=sumof(n);
             cout<<n<<" ";
-            if(n==1)return true;
-            if(n<=9 && c<=2)
-            {
-                c++;
-            }else if(n<=9)
+            if(n==1 ||n==7)return true;
+            if(n<=9)
             {
                 return false;
             }
