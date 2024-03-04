95a3d831be09dbaffa2587686d1c60ff9c13d30a
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-03-04 15:25:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (9.02%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int>sb;
+    int a=0,b=1;
+    int fibo(int n)
+    {
+        if(sb[n]==-1)
+        {
+            int res;
+            if(n==0 || n==1)return n;
+            else
+            {
+                res =fibo(n-1)+fibo(n-2);
+            }
+            sb[n]=res;
+        }
+        return sb[n];
+
+    }
+    int fib(int n) {
+        if(sb.size()<n+1)
+        {
+            sb.assign(n+1,-1);
+        }
+       return fibo(n);
+    }
+};
