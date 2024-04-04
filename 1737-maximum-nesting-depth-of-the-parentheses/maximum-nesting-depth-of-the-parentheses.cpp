189e7bae786c48f16811eb4f5a52ba35034626f9
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-04-04 21:27:44 +0530

    Time: 3 ms (40.00%) | Memory: 7.3 MB (96.44%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char>st;
-        vector<int>v,temp;
-        int c=0,k=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(st.empty() && s[i]=='(')
-            {
-                st.push(s[i]);
+        if(s=="") return 0;
+        int mxDepth=0;
+        int cntOpen=0;
+        for( int i=0;i<s.size();i++){
+
+            if(s[i]=='('){
+                cntOpen++;
+                mxDepth=max(mxDepth,cntOpen);
+            }
+            else if(s[i]==')'){
+             cntOpen--;
             }
             else{
-                if(s[i]=='(')
-                {
-                    if(c>=1)
-                    {
-                        int a=c+st.size();
-                        k=max(a,k);
-                        c=0;
-                    } 
-                    st.push('(');
-                }else if(s[i]==')')
-                {
-                    st.pop();
-                    c++;
-                    if(st.empty()){
-                        k=max(c,k);
-                        c=0;
-                    }
-                }
-
+               continue;
             }
         }
-        return k;
+       return mxDepth; 
     }
 };
