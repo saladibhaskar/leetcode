2b2412669ded98e90b0add32f3a3203a1d4385c5
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-04-05 20:45:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (24.89%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     string makeGood(string s) {
        int i=0,c=0;
-       string st=s;
         while(i<s.size()-1 && !s.empty())
         {
-            int a=s[i];
-            int b=s[i+1];
-            cout<<a<<b<<" ";
-            if(a==b+32 ||b==a+32)
+            int a=s[i],b=s[i+1];
+            if(a==b+32 || b==a+32)
             {
                 s.erase(i,2);
                 c++;
             }
-            else
-            {
-                i++;
-            }
+            else i++;
         }
         if(c==0) return s;
         else return makeGood(s);
