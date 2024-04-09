dbfef93a4817c4af2c77d4461456a2984c39cd6a
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-04-09 22:15:09 +0530

    Time: 5 ms (22.66%) | Memory: 9.5 MB (46.11%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -4,18 +4,9 @@ public:
         int fr=0,c=0;
         while(t[k]!=0)
         {
-            if(t[fr]!=0)
-            {
-                t[fr]--;
-                fr++;
-                c++;
-            }else
-            {
-                fr++;
-
-            }
+            if(t[fr]!=0) t[fr]--,fr++,c++;
+            else fr++;
             if(fr==t.size())fr=0;
-
         }
         return c;
     }
