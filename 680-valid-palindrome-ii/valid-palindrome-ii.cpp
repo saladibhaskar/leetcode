3ef7c581d443b21b124b683e36eef6979e52ae87
Bhaskar saladi <119723167+saladibhaskar@users.noreply.github.com>
2024-05-14 01:17:53 +0530

    Time: 46 ms (93.56%) | Memory: 25.5 MB (44.80%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -4,10 +4,8 @@ public:
     {
         while(i<j)
         {
-            if(s[i]==s[j])
-            {
-                i++,j--;
-            }else return false;
+            if(s[i]!=s[j]) return false;
+            i++,j--;
         }
         return true;
     }
@@ -15,12 +13,8 @@ public:
         int  i=0,j=s.size()-1;
         while(i<j)
         {
-            if(s[i]!=s[j])
-            {
-                return check(s,i+1,j) || check(s,i,j-1);
-            }
-                i++;
-                j--;
+            if(s[i]!=s[j]) return check(s,i+1,j) || check(s,i,j-1);
+                i++,j--;
         }
         return true;
  
