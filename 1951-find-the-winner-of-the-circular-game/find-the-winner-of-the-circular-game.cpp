0805cc9d51bea54a51d66364ac5e1d24110066df
Bhaskar saladi <22MH1A42I8@acoe.edu.in>
2024-07-08 21:21:08 +0530

    Time: 4 ms (53.80%) | Memory: 7.3 MB (55.91%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        queue<int>q;
-        int i=1;
-         for( int i=1;i<=n;i++)
-         {
-            q.push(i);
-         }
-         while(q.size()!=1)
-         {
-            if(i==k)
-            {
-                q.pop();
-                i=0;
-            }
-            else
-            {
-                int e=q.front();
-                q.pop();
-                q.push(e);
-            }
-            i++;
-         }
-         return q.front();
+        vector<int>v(n);
+        iota(v.begin(),v.end(),1);
+        int i=0,s=0;
+        while(v.size()!=1)
+        {  
+            s=(s+k-1)%v.size();
+            cout<<s;
+            v.erase(v.begin()+s);
+        }
+        return v[0];
     }
 };
