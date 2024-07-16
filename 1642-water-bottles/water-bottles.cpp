aebbe04dc070776a06eccd33193200a98dfec17c
Bhaskar saladi <22MH1A42I8@acoe.edu.in>
2024-07-16 21:21:43 +0530

    Time: 2 ms (53.85%) | Memory: 7.3 MB (37.18%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int drinked=numBottles;
+        int empty=numBottles;
+        while(empty>=numExchange)
+        {
+            drinked+=(empty/numExchange);
+            empty=empty/numExchange+empty%numExchange;
+        }
+        return drinked;
+    }
+};
