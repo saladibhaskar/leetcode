ad2baa78ff924cd175b40f849bca6f86055e6951
Bhaskar saladi <22MH1A42I8@acoe.edu.in>
2025-02-01 21:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (25.00%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(abs(nums[i]-nums[i+1])%2==0)return false;
+        }
+        return true;
+        
+    }
+};
