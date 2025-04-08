0a368379e53da214498002ed9ced8c9097ddaae8
Bhaskar saladi <22MH1A42I8@acoe.edu.in>
2025-04-08 14:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (96.93%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        int c=0;
-        for( int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]+=1;
+        bool e[101] = {0};
+        int size = nums.size();
+
+        for (int i = size - 1; i >= 0; --i) {
+            if (e[nums[i]])
+                return ceil(double(i + 1) / 3);
+            e[nums[i]] = true;
         }
-        if(mp.size()==nums.size())
-        {
-            {
-                return 0;
-            }
-        }else
-        {
-            int i=0;
-            while(i<nums.size())
-            {
-                if(nums.size()-i>=3)
-                {
-                    c+=1;
-                    mp[nums[i]]-=1;
-                    mp[nums[i+1]]-=1;
-                    mp[nums[i+2]]-=1;
-                    if(mp[nums[i]]==0)mp.erase(nums[i]);
-                    if(mp[nums[i+1]]==0)mp.erase(nums[i+1]);
-                    if(mp[nums[i+2]]==0)mp.erase(nums[i+2]);
-                    i+=3;
-                }
-                else{
-                    return c+1;
-                }
-                if(mp.size()==nums.size()-i)return c;
-            }
-        }
-       return c; 
+        
+        return 0;
     }
 };
